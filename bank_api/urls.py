284086a190e45e83a5fe5128558082338a8ede72
SaurabhSB07 <barkesaurabh07@gmail.com>
2025-12-06 00:16:14 +0530

    Added root view and updated ALLOWED_HOSTS

--- bank_api/urls.py
+++ bank_api/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import home  # Make sure this import matches step 1
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
+    path('', home),  # Root URL now works
 ]
